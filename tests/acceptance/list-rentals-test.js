af6902d7446ee491f9bd8ca738620a3d2d5c0c20
Chithambaram N <chithambaram.n@cliffex.com>
2019-11-21 16:38:57 +0530

    Test for Model Page

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -5,15 +5,8 @@ import { setupApplicationTest } from 'ember-qunit';
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('should link to information about the company', async function(assert) {
+  test('should list available rentals.', async function(assert) {
     await visit('/');
-    await click(".menu-about");
-    assert.equal(currentURL(), '/about', 'should navigate to about');
-  });
-
-  test('should link to contact information', async function(assert) {
-    await visit('/');
-    await click(".menu-contact");
-    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+    assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
   });
 });
