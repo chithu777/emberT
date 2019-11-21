720a7d66c1ebc59c257dfd9413e9dd5c85413616
Chithambaram N <chithambaram.n@cliffex.com>
2019-11-21 17:31:38 +0530

    model setting attr()

--- app/models/rental.js
+++ app/models/rental.js
@@ -1,5 +1,11 @@
-import Model from '@ember-data/model';
-
+import DS from '@ember-data';
+const { Model } = DS;
 export default Model.extend({
-
+  title: DS.attr(),
+  owner: DS.attr(),
+  city: DS.attr(),
+  category: DS.attr(),
+  image: DS.attr(),
+  bedrooms: DS.attr(),
+  description: DS.attr()
 });
